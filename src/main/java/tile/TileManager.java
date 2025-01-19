21e286cdb2da6fd86c57dad2ba8b1c9b2c5b96a8
Buoyy <saarthak0902@gmail.com>
2025-01-19 19:57:19 +0530

    Map update with dirt-y textures

--- src/main/java/tile/TileManager.java
+++ src/main/java/tile/TileManager.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 import javax.imageio.ImageIO;
 
@@ -12,11 +13,11 @@ import main.GamePanel;
 
 public class TileManager {
     private final GamePanel gp;
-    private Tile[] tiles; // all kinds of tiles (like water, grass, brick)
-    private int[][] mapTileNum; // to load the number assigned to a tile from map (like 0 for brick)
+    private final Tile[] tiles; // all kinds of tiles (like water, grass, brick)
+    private final int[][] mapTileNum; // to load the number assigned to a tile from map (like 0 for brick)
     public TileManager(GamePanel gp) {
         this.gp = gp;
-        tiles = new Tile[4]; // Currently. we have 3 types of tiles. Just to be safe, we have 4 spaces for tiles. 
+        tiles = new Tile[8]; // Currently. we have 3 types of tiles. Just to be safe, we have 4 spaces for tiles.
         mapTileNum = new int[gp.maxScreenCols][gp.maxScreenRows]; // the 2D array needs to be limited to the screen's dimensions. 
         
      // Next we load in our map data and tile data.
@@ -27,7 +28,7 @@ public class TileManager {
         try {
          // Method: we load the map's input stream to read it from a reader. By a loop, we read a line and split the numbers into an array and pass the array's data into mapTileNum as (col, row) in coordinate form. 
 
-            InputStream is = getClass().getResourceAsStream("/maps/testmap.txt");
+            InputStream is = getClass().getResourceAsStream("/maps/test_map.txt");
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
             int col = 0; int row = 0;
             while (col < gp.maxScreenCols && row < gp.maxScreenRows) {
@@ -52,13 +53,29 @@ public class TileManager {
      // Load in all images for different kinds of tiles
         try {
             tiles[0] = new Tile();
-            tiles[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/brick.png"));
+            tiles[0].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/brick.png")));
             
             tiles[1] = new Tile();
-            tiles[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass.png"));
+            tiles[1].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/grass.png")));
 
             tiles[2] = new Tile();
-            tiles[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water.png"));
+            tiles[2].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/water.png")));
+
+            tiles[3] = new Tile();
+            tiles[3].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/dirty_grass.png")));
+
+            tiles[4] = new Tile();
+            tiles[4].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/dirty_grass_left.png")));
+
+            tiles[5] = new Tile();
+            tiles[5].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/dirty_grass_left_end.png")));
+
+            tiles[6] = new Tile();
+            tiles[6].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/dirty_grass_right.png")));
+
+            tiles[7] = new Tile();
+            tiles[7].image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/tiles/dirty_grass_right_end.png")));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
